67ae9321c018c04b8711214bc67941c4d12c69fe
Wesley Egbertsen <wesleyegbertsen@gmail.com>
2026-01-04 23:09:27 +0100

    Fixed missing default languages `pl-PL` & `ru-RU` in `LocalizationService`

--- src/main/java/com/zetaplugins/timberz/service/LocalizationService.java
+++ src/main/java/com/zetaplugins/timberz/service/LocalizationService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public final class LocalizationService {
     private final JavaPlugin plugin;
-    public static final List<String> defaultLangs = List.of("en-US", "de-DE", "nl-NL");
+    public static final List<String> defaultLangs = List.of("en-US", "de-DE", "pl-PL", "ru-RU", "nl-NL");
 
     private FileConfiguration langConfig;
 
